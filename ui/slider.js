6b570746bd552a468a8681a5c9d7625fec79c2c4
Q <life.qm@gmail.com>
2015-05-21 13:30:28 +0800

    添加Q.Slider滑块接口文档

--- ui/slider.js
+++ ui/slider.js
@@ -24,7 +24,19 @@ function find_item(root, cmp) {
   return null;
 }
 
-Q.slider = Q.extend({
+/**
+ * @constructor
+ * 
+ * @param {Object} config - 构造参数
+ * @param {string} [config.type=vt] - 滑块类型， 水平(hr)或者垂直(vt) 默认
+ * @param {number} [config.max=100] - 值域上限
+ * @param {number} [config.min=0] - 值域下限
+ * @param {number} [config.duration=1] - 滑动步长
+ * @param {value}  [config.value=0] - 初始值
+ * @param {Q.Slider.callback=} config.on_xscroll - 水平滑动监听回调   
+ * @param {Q.Slider.callback=} config.on_yscroll - 水平滑动监听回调   
+ */
+Q.Slider = Q.extend({
   min : 0,
   max : 100,
   value : 0,
@@ -49,7 +61,7 @@ Q.slider = Q.extend({
     this.thumb = find_item(this.hwnd, function(e) { return (e && e.nodeType == Q.ELEMENT_NODE) && (e.className.indexOf('q-thumb') >= 0); });
       
     if(this.thumb) {
-      Q.drag.attach_object(this.thumb, {self: true, onmove: function(x, y) {
+      Q.drag({id: this.thumb, self: true, onmove: function(x, y) {
         if(_this.type != 'hr') {
           if(y < 0) 
             y = 0;
@@ -68,15 +80,25 @@ Q.slider = Q.extend({
           _this.thumb.style.left = x + 'px';
         }
       }});
-      _this.set_value(config.value);
+      _this.setValue(config.value);
     }
   },
-
-  get_value : function() {
+  
+  /** 获取当前滑块的值 
+   * 
+   * @memberof Q.Slider.prototype
+   * @returns {number}  当前滑块值
+   */
+  getValue : function() {
     return this.value;
   },
 
-  set_value : function(value) {
+  /** 设置滑块值, 设置后会触发滑块事件
+   *
+   * @memberof Q.Slider.prototype
+   * @param {number} value - 值
+   */
+  setValue : function(value) {
     var _this = this;
     if(value > _this.max) {
       _this.value = _this.max;
@@ -94,11 +116,14 @@ Q.slider = Q.extend({
     } else {
       var x = Math.floor(f*(_this.hwnd.offsetWidth-_this.thumb.offsetWidth));
       _this.on_xscroll(_this.value);
-      alert(x)
       _this.thumb.style.left = x + 'px';
     }
   },
-
+  
+  /** 滑动回调
+   * @callback Q.Slider.callback
+   * @param {number} value - 当前滑动的值
+   */
   on_xscroll : function(value) {}, 
   on_yscroll : function(value) {}
 }); 
